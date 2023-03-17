2457783cc6af47b9141cb9c6409db591c59fef90
Siriuz <72621548+thirdyraza@users.noreply.github.com>
2023-03-17 15:11:58 +0800

    minor adjustments

--- Third/Assets/slider.js
+++ Third/Assets/slider.js
@@ -2,19 +2,19 @@
 const testimonials = [
   {
     name: "Project Management",
-    job: "Successfully plan, execute, and deliver projects on time and within budget. We understand your specific project needs and objectives, and we develop customized project plans and timelines that align with the your goals.",
+    job: "Successfully plan, execute, and deliver projects on time and within budget. We understand your specific project needs and objectives, and we develop customized project plans and timelines that we know will align with the your goals.",
     image: "Pictures/project.png",
     
   },
   {
     name: "Computer and Supplies",
-    job: "Run computer systems effectively with the right hardware and accessories. The quality and reliability of the computer and supplies are our topmost priority and is the key to your satisfaction.",
+    job: "Run computer systems effectively with the right hardware and accessories. The quality and reliability of the computer and supplies are our topmost priority, because we believe it plays a vital part towards your satisfaction and success.",
     image: "Pictures/supplies.png",
     
   },
   {
     name: "Consulting Services",
-    job: "Improve performance, increase efficiency, reduce costs, enhance organizational structure, or implement new initiatives. We deliver value to our clients, and we provide expert advice and guidance that can help you achieve your goals and objectives.",
+    job: "Improve performance, increase efficiency, reduce costs, enhance organizational structure, or implement new initiatives. We deliver value to our clients, expert advice, and guidance that can help you achieve your goals and objectives.",
     image: "Pictures/consult.png",
     
   },
@@ -26,7 +26,7 @@ const testimonials = [
   },
   {
     name: "Training and Development",
-    job: "Improve knowledge, skills, and competencies to achieve goals and objectives. To enhance the capabilities of individuals and organizations, promote continuous learning and development, and support the achievement of strategic goals and objectives, we are here for you.",
+    job: "Improve knowledge, skills, and competencies to achieve goals and objectives. To enhance the capabilities of individuals and organizations, promote continuous learning and development, and support the achievement of strategic goals and objectives.",
     image: "Pictures/training.png",
     
   },
